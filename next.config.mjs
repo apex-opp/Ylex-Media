737a5d102bb47b681a1c09084cd0e82cb91cfda6
apex-opp <devikinki085@gmail.com>
2026-05-31 17:12:56 +0530

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // SPA-ish behavior: we keep everything client-side, but still benefit from Next's routing and bundling.
-  // If you later add server actions/routes, this config won't fight you.
-};
-
-export default nextConfig;
